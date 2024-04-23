b8271eefaefa3c29a31c9768900547ac90ac0938
Nick Ward <nick@klarventures.onmicrosoft.com>
2024-04-23 11:07:03 -0400

    remove bootstrap components

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash, callback, html, dcc
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
